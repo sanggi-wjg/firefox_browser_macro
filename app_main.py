d4d31bbb394a83bfadcee974578333873e08e8a4
jay_g <jay_g@kr.accommate.com>
2020-08-31 15:47:03 +0900

    2020 08 31

--- app_main.py
+++ app_main.py
@@ -9,17 +9,25 @@ from apps.settings import FIREFOX_PATH, POSITIONS_JSON, PROPERTIES_JSON
 pyinstaller -F -n print_label_macro --icon=assets/ozoz.ico  app_main.py
 """
 
+
+def get_json_files(json_file_path: str):
+    if not os.path.exists(json_file_path):
+        raise FileNotFoundError(json_file_path + ' 파일이 없습니다.')
+
+    with open(json_file_path) as json_file:
+        contents = json.load(json_file)
+
+    print(json_file_path + ' 을(를) 불러왔습니다.')
+    return contents
+
+
 if __name__ == '__main__':
     try:
-        if not os.path.exists(POSITIONS_JSON): raise FileNotFoundError(POSITIONS_JSON + ' 파일이 없습니다.')
-        if not os.path.exists(PROPERTIES_JSON): raise FileNotFoundError(PROPERTIES_JSON + ' 파일이 없습니다.')
-        if not os.path.exists(FIREFOX_PATH): raise FileNotFoundError('Firefox.exe 파일이 없습니다.\n바탕화면에 Firefox Portable 을 설치해주세요.')
+        if not os.path.exists(FIREFOX_PATH):
+            raise FileNotFoundError('Firefox.exe 파일이 없습니다.\n바탕화면에 Firefox Portable 을 설치해주세요.')
 
-        with open(POSITIONS_JSON) as position_json:
-            positions = json.load(position_json)
-
-        with open(PROPERTIES_JSON) as properties_json:
-            properties = json.load(properties_json)
+        positions = get_json_files(POSITIONS_JSON)
+        properties = get_json_files(PROPERTIES_JSON)
 
         firefox = FirefoxDriver(positions = positions, properties = properties)
         firefox.run()
